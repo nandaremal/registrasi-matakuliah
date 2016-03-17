6fb58c8f2254c3ad14f621ce633bb3d52840abd7
rizkyafrizalaw <rizkyafrizal72@gmail.com>
2016-03-17 11:31:43 +0700

    Update Dosen.java

--- Registrasi_Matakuliah/src/registrasi_matakuliah/Dosen.java
+++ Registrasi_Matakuliah/src/registrasi_matakuliah/Dosen.java
@@ -10,13 +10,29 @@ package registrasi_matakuliah;
  * @author Rizky_Afrizal_AW
  */
 public class Dosen {
-    private String Nip;
+	private int i; 
+	private String kode;
+	private String Nip;
 	private String Nama;
-}
-
-Dosen(String Nip, String Nama){
+	private Kelas[] nKelas;
+	
+public Dosen(String Nip, String Nama){
 	this.Nip=Nip;
 	this.Nama=Nama;
+	nKelas=new Kelas[5];
+	i=5;
+	
+}
+
+public Dosen(String Nip, String Nama, int i){
+	this.Nip=Nip;
+	this.Nama=Nama;
+	this.i=i;
+	nKelas=new Kelas[i];
+}
+
+public void seti(int i){
+	this.i=i;
 }
 
 public void setNip(String Nip){
