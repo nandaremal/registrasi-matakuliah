d4329464c62d51da32a1563cda08d2f040388608
Nuzulul Aulia <nuzulul19@gmail.com>
2016-03-19 15:36:38 +0700

    Update Constructor dan void removeKelas

--- Registrasi_Matakuliah/src/registrasi_matakuliah/Mahasiswa.java
+++ Registrasi_Matakuliah/src/registrasi_matakuliah/Mahasiswa.java
@@ -26,18 +26,22 @@ public class Mahasiswa extends Orang
 		return nim;
 	}
 	
-	public Mahasiswa(String nama, String nim)
+	public Mahasiswa(String nama, String nim, String jenisk, String alamat)
 	{
 		super(nama);
 		this.nim = nim;
+		super(jenisk);
+		super(alamat);
 		pilihan = new Kelas[5]; //Default max Kelas = 5, jika tidak di set
 		maxKelas = 5;
 	}
 	
-	public Mahasiswa(String nama, String nim, int maxKelas)
+	public Mahasiswa(String nama, String nim, String jenisk, String alamat, int maxKelas)
 	{
 		super(nama);
 		this.nim = nim;
+		super(jenisk);
+		super(alamat);
 		this.maxKelas = maxKelas;
 		pilihan = new Kelas[maxKelas];
 	}
@@ -50,16 +54,15 @@ public class Mahasiswa extends Orang
 		}
 	}
 	
-	public void removeKelas(String namaMatkul)
+	public void removeKelas(String namaMatkul) 
 	{
-	/*	if(namaMatkul == pilihan[nKelas].getMatakuliah())
-		{	
-			this.pilihan[nKelas].setMatakuliah("","");
-		} else
+		while (nKelas = 0; nKelas <= maxKelas; nKelas++)
 		{
-			nKelas++;
+			if(namaMatkul == pilihan[nKelas].getMatakuliah())
+			{	
+				this.pilihan[nKelas].setMatakuliah(Null);
+			}
 		}
-	*/ 
 	}
 	
 	public int getJumlahKelas()
